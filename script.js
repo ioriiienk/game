3269d896067490343695f118bd2dacbdc3599619
Shohel Rana <97423280+ioriiienk@users.noreply.github.com>
2022-02-20 19:08:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+var won,lost,match;
+
+match = 1;
+won = 0;
+lost = 0;
+
+for(i = 1; i <=3; i++ ){
+
+    var guess = Number(prompt("Please Guess Number From 1 To 5"));
+
+    var random = Math.floor(Math.random() * 5 + 1);
+
+    if( guess == random ){
+        document.write("<br> Match = " + match);
+        document.write("<br> Your Guessed Number is = " + guess + "<br>" + "Correct Number is = " + random + "<br>");
+        document.write("Game Result = " + "<p class = won >You Won!</p> <br>");
+        won++;
+        match++
+    }
+    
+    else{
+        document.write("<br>Match = " + match);
+        document.write("<br> Your Guessed Number is = " + guess + "<br>" + "Correct Number is = " + random + "<br>");
+        document.write("Game Result = " + "<p class = lost >You Are Lost!</p> <br>");
+        lost++;
+        match++;
+    };
+}
+
+
+document.write("<br><br>" + "<h3>Game Result</h3>");
+document.write("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "You Had Won = " + won + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
+document.write("You Had Lost = " + lost + "<br><h6></h6>");
